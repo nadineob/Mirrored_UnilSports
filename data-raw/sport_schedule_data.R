a85908f4fb56fc49ae46bc886f507d40c3fa912c
arturoglb <arturo.glb@hotmail.com>
2022-12-02 18:50:35 +0100

    Updating web scrapping code to create masive collection for sports schedule

--- data-raw/sport_schedule_data.R
+++ data-raw/sport_schedule_data.R
@@ -63,7 +63,7 @@ sport_schedule.today <- tibble(data.frame(
 #----------------------------------------------------------------------------
 # Retrieve 8 days of Unil Sports Center
 
-# Test
+# Retrieve the links for each of the days shown on the website
 schedule.html <- read_html("https://sport.unil.ch/?mid=92") # webpage
 a_elements <- schedule.html %>% 
   html_nodes(".summary .level2>li>a") %>% 
@@ -74,6 +74,7 @@ a_elements <- paste("https://sport.unil.ch/",a_elements, sep = "")
 table_new <-data.frame()
 sport_schedule <- data.frame()
 
+# Create sports table with 8 dates
 for (i in 1:8) {
   html <- read_html(a_elements[i])
   table_new <- rvest::html_element(html, css = "table.quotidien" ) %>% 
@@ -92,6 +93,68 @@ for (i in 1:8) {
   sport_schedule<- rbind(sport_schedule,table_new) # Create table
 }
 
+#-------------------------------------------------------------------------------
+
+# Retrieve 60 days of Unil Sports Center
+
+# Adding today's link to the list
+schedule.html <- read_html("https://sport.unil.ch/?mid=92") # webpage
+a_elements <- schedule.html %>% 
+  html_nodes(".summary .level2>li>a") %>% 
+  html_attr("href")
+a_elements <- paste("https://sport.unil.ch/",a_elements, sep = "") 
+
+#Create list of links for the master table
+new_list <- list()
+
+# Adding today's link to the new list
+new_list <- append(new_list,a_elements[1])
+
+
+# Create list of the up coming 60 days
+for (i in 1:60) {
+  if (i == 1) {
+    schedule.html <- read_html("https://sport.unil.ch/?mid=92") # webpage
+    a_element <- schedule.html %>% 
+      html_nodes(".planning-navigation .next>a") %>% 
+      html_attr("href")
+    new.html <- paste("https://sport.unil.ch/",a_element, sep = "") 
+    new_list <- append(new_list,new.html)
+  } else {
+    schedule.html <- read_html(new.html) # webpage
+    a_element <- schedule.html %>% 
+      html_nodes(".planning-navigation .next>a") %>% 
+      html_attr("href")
+    new.html <- paste("https://sport.unil.ch/",a_element, sep = "") 
+    new_list <- append(new_list,new.html)
+  }
+}
+
+# Transform to a vector of characters
+new_vector <- unlist(new_list)
+    
+# Creating two empty dataframes for table
+table_new <-data.frame()
+sport_schedule <- data.frame()
+
+# Create sports table with 61 dates
+for (i in 1:61) {
+  html <- read_html(new_vector[i])
+  table_new <- rvest::html_element(html, css = "table.quotidien" ) %>% 
+    rvest::html_table() %>% 
+    slice(-1) %>% # Remove first Row (weird names) 
+    rename("Timetable" = "X1", "Activity"= "X2", "Location"="X3")
+  previous.date <- html %>%
+    html_nodes("div.planning-navigation td.back") %>% # Retrieve the yesterdays date
+    html_text2()
+  previous.date <- gsub("\\.", "-", previous.date)
+  previous.date <- parse_date_time(previous.date, orders = c("ymd", "dmy", "mdy"))
+  todays.date <- as.Date(previous.date) + 1 # Retrieve todays date
+  table_new <-  table_new %>% 
+    mutate(Date = todays.date) %>% 
+    relocate(Date, .before = Timetable)
+  sport_schedule<- rbind(sport_schedule,table_new) # Create table
+}
+
 # Create data to use for the functions
 usethis::use_data(sport_schedule, overwrite = TRUE) # create data file
-
