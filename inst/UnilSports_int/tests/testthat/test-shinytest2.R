8a19295c9569e5415d81abacdd76159369b096d6
nadineob <100680357+nadineob@users.noreply.github.com>
2022-12-19 20:28:33 +0100

    updated shiny tests to pass Github check

--- inst/UnilSports_int/tests/testthat/test-shinytest2.R
+++ inst/UnilSports_int/tests/testthat/test-shinytest2.R
@@ -1,20 +1,15 @@
 library(shinytest2)
 
-test_that("{shinytest2} recording: test_optim", {
-  app <- AppDriver$new(variant = platform_variant(), 
-                       name = "test_optim", 
-      height = 859, width = 1619)
+
+test_that("{shinytest2} recording: test_app", {
+  app <- AppDriver$new(variant = platform_variant(), name = "test_app", height = 859, 
+      width = 1619)
   app$set_inputs(date = "2022-12-20")
-  app$set_inputs(time = "09:00 – 10:00")
-  app$set_inputs(time = c("09:00 – 10:00", "11:00 – 12:00"))
-  app$set_inputs(time = c("09:00 – 10:00", "11:00 – 12:00", "12:00 – 13:00"))
-  app$set_inputs(time = c("09:00 – 10:00", "11:00 – 12:00", "12:00 – 13:00", 
+  app$set_inputs(time = c("08:00 – 09:00", "10:00 – 11:00", "12:00 – 13:00", 
       "14:00 – 15:00"))
-  app$set_inputs(activity = c("Basketball / Pratique libre à l'extérieur", 
-                              "Football / Pratique libre", 
-      "Musculation connectée / 1. Introduction"), wait_ = FALSE)
-  app$set_inputs(no_dup = TRUE, wait_ = FALSE)
+  app$set_inputs(activity = c("Basketball / Pratique libre à l'extérieur", "Football / Pratique libre"))
+  app$set_inputs(no_dup = TRUE)
   app$click("opt")
-  app$expect_screenshot()
   app$expect_values()
+  app$expect_screenshot()
 })
